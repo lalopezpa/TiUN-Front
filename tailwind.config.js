0228a42d83a028aec152aa3384e7970b08da1840
angelvasquez06 <vasquezgerenaangelmateo@gmail.com>
2023-08-15 18:58:44 -0500

    Se añade el eslint configurado, así como variables del tailwindconfig.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: 'media',
+	theme: {
+		extend: {
+			colors: {
+				verdeClaro: '#85CF62',
+				verdeOscuro: '#063500',
+				vinotinto: '#A30D24',
+				gris: '#58595B',
+				amarillo: '#F3A434',
+			},
+		},
+	},
+	plugins: [],
+};
