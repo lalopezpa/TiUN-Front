e5bd88789b2664b838f1579e258d467d9c33cfdf
lalopezpa <lalopezpa@unal.edu.co>
2023-08-26 15:31:00 -0500

    Acomodacion elementos del footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {GithubIcon, InstagramIcon, FacebookIcon} from '../icons/icons';
 
 const Footer = () => (
-	<footer className='bg-black bg-opacity-70 text-white py-6 flex items-center justify-end'>
+	<footer className='bg-black bg-opacity-70 text-white py-5 flex items-center justify-end '>
 		<div className='flex items-center'>
 			<button className='mr-2'>
 				<GithubIcon />
@@ -14,8 +14,11 @@ const Footer = () => (
 			<button className='mr-2'>
 				<InstagramIcon />
 			</button>
-			<p className='text-white'>Términos y condiciones</p>
-			<p className='text-white'>TiUN® 2023-2023 Todos los derechos reservados</p>
+			<div className='border-r-2 border-amarillo h-9 mr-2'></div> {/* Línea vertical */}
+			<div className='mr-2'>
+				<p className='text-white'>Términos y condiciones</p>
+				<p className='text-white'>TiUN® 2023-2023 Todos los derechos reservados</p>
+			</div>
 		</div>
 	</footer>
 );
