b01c24f843fa52d2228bf55c4128ac78e4a6c6ad
angelvasquez06 <vasquezgerenaangelmateo@gmail.com>
2023-09-01 18:46:04 -0500

    cambios iniciales pagina de registro

--- src/pages/Auth/register.tsx
+++ src/pages/Auth/register.tsx
@@ -1,50 +1,22 @@
 // Register.tsx
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-
+import DarkModeToggle from '../../components/common/DarkModeToggle';
+import useDarkMode from '../../hooks/useDarkMode';
 const Register = () => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [confirmPassword, setConfirmPassword] = useState('');
-
-	const handleRegister = (event: React.FormEvent) => {
-		event.preventDefault();
-		// Implement your registration logic here
-		// You can use the 'email' and 'password' state values for registration
-	};
-
+	const {modoOscuro, toggleModoOscuro} = useDarkMode();
 	return (
-		<div>
-			<h2>Register</h2>
-			<form onSubmit={handleRegister}>
-				<div>
-					<label>Email:</label>
-					<input type='email' value={email} onChange={e => {
-						setEmail(e.target.value);
-					}} />
+		<>
+			<header className='fixed top-0 left-0 w-full p-4 bg-transparent z-50'>
+				<div className='flex justify-between'>
+					<Link to='/home' className='text-white  '>Inicio</Link>
+					<DarkModeToggle modoOscuro={modoOscuro } toggleModoOscuro={toggleModoOscuro} />
 				</div>
-				<div>
-					<label>Password:</label>
-					<input type='password' value={password} onChange={e => {
-						setPassword(e.target.value);
-					}} />
-				</div>
-				<div>
-					<label>Confirm Password:</label>
-					<input
-						type='password'
-						value={confirmPassword}
-						onChange={e => {
-							setConfirmPassword(e.target.value);
-						}}
-					/>
-				</div>
-				<button type='submit'>Register</button>
-			</form>
-			<p>
-        Already have an account? <Link to='/login'>Login</Link>
-			</p>
-		</div>
+			</header>
+			<main>
+				<h1>hola</h1>
+			</main>
+		</>
 	);
 };
 
