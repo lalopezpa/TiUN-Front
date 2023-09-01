30cce8a16e19d01a7ebe0665d0cd8af302e3caf5
angelvasquez06 <vasquezgerenaangelmateo@gmail.com>
2023-09-01 16:11:12 -0500

    Correción nombre de los assets y sus imports

--- src/pages/Auth/login.tsx
+++ src/pages/Auth/login.tsx
@@ -1,14 +1,15 @@
 // Login.tsx
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
-import fondo from '../../assets/fondo.jpg';
-import logo from '../../assets/Logotipo..png';
+import logo from '../../assets/logo.png';
+import logooscuro from '../../assets/logo_oscuro.png';
 import {useForm} from 'react-hook-form';
 import type RequestData from '../../types/RequestData';
 import FooterLogin from '../../components/common/FooterLogin';
 import DarkModeToggle from '../../components/common/DarkModeToggle';
 import useDarkMode from '../../hooks/useDarkMode';
 import {useAuth} from '../../context/authContext';
+import Background from '../../components/common/Background';
 
 const Login = (): JSX.Element => {
 	const	{register, handleSubmit, formState: {errors}, watch} = useForm();
@@ -27,23 +28,23 @@ const Login = (): JSX.Element => {
 			<header className='fixed top-0 left-0 w-full p-4 bg-transparent z-50'>
 				<div className='flex justify-between'>
 					<Link to='/home' className='text-white  '>Inicio</Link>
-					<DarkModeToggle modoOscuro={modoOscuro} toggleModoOscuro={toggleModoOscuro} />
+					<DarkModeToggle modoOscuro={modoOscuro } toggleModoOscuro={toggleModoOscuro} />
 				</div>
 			</header>
 			<main className='flex min-h-screen bg-verdeClaro bg-opacity-75 dark:bg-verdeOscuro md:full'>
 				{/* División izquierda */}
 				<section className='w-1/2 flex flex-col justify-center items-center overflow-hidden min-h-max relative hidden md:flex sm:w-100%'>
 					<div className='w-full h-full absolute top-0 left-0 z-10'>
-						<img src={fondo} alt='fondobolsas' className='w-full h-full opacity-5 bg-cover' />
+						<Background modoOscuro={modoOscuro} />
 					</div>
 					<figure className='z-20'>
-						<img src={logo} alt='logo' className='w-[24rem] h-[24rem] px-4 lg:w-[36rem] lg:h-[36rem] lg:px-0' />
+						<img src={modoOscuro ? logooscuro : logo} alt='logo'loading='lazy' className='w-[24rem] h-[24rem] px-4 lg:w-[36rem] lg:h-[36rem] lg:px-0' />
 					</figure>
 				</section>
 				{/* División derecha */}
 				<section className='bg-verdeSeccionLogin bg-opacity-90 w-full md:w-1/2 p-10 pb-0 flex flex-col justify-between items-center md:pt-48 dark:bg-verdeOscuro'>
 					<figure className='flex md:hidden'>
-						<img src={logo} alt='logo' className='w-48 h-48 pb-5' />
+						<img src={modoOscuro ? logooscuro : logo} alt='logo' loading='lazy' className='w-48 h-48 pb-5' />
 					</figure>
 					<form onSubmit={handleSubmit(onSubmit)} className='w-full text-xl flex flex-col justify-center items-center'>
 						<h2 className='text-5xl mb-4 font-poppins font-bold text-white'>INICIO DE SESIÓN</h2>
