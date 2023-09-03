4a7971951332a98f7ade786c4035962d60f2ca99
lalopezpa <lalopezpa@unal.edu.co>
2023-09-03 17:27:23 -0500

    cambio bordes carrusel

--- src/components/common/Carrusel.tsx
+++ src/components/common/Carrusel.tsx
@@ -3,16 +3,16 @@ import {Carousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Carrusel: React.FC = () => (
-	<div className='w-3/4 mx-auto'>
+	<div className='w-3/4 mx-auto z-10 bg-white border-gray-200 rounded-lg shadow-lg '>
 		<Carousel showThumbs={false}>
 			<div>
-				<img className='rounded-md' src='https://via.placeholder.com/550x200' alt='Imagen 1' />
+				<img className='rounded-lg' src='https://via.placeholder.com/550x200' alt='Imagen 1' />
 			</div>
 			<div>
-				<img className='rounded-md' src='https://via.placeholder.com/550x200' alt='Imagen 2' />
+				<img className='rounded-lg' src='https://via.placeholder.com/550x200' alt='Imagen 2' />
 			</div>
 			<div>
-				<img className='rounded-md' src='https://via.placeholder.com/550x200' alt='Imagen 3' />
+				<img className='rounded-lg' src='https://via.placeholder.com/550x200' alt='Imagen 3' />
 			</div>
 			{/* Agrega más imágenes según sea necesario */}
 		</Carousel>
