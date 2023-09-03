d534a26aaf894dd9bd54ffac04fb136af92b3579
lalopezpa <lalopezpa@unal.edu.co>
2023-09-03 17:29:47 -0500

    Se añaden las cards al home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,24 +9,60 @@ import Card from '../components/common/Card';
 
 const Container = styled.div`
   padding-top: 65px;
+  display:flex;
+  flex-wrap: wrap; /* Permite que las tarjetas fluyan hacia la siguiente línea cuando no haya suficiente espacio horizontal */
+  justify-content: center; /* Centra las tarjetas horizontalmente */
+  gap: 1rem; /* Espacio entre las tarjetas */
 `;
 
 const Title = styled.h1`
-  margin-top: 1rem;
+  margin-top: 2rem;
   text-align: center;
   font-size: 1.5rem;
   margin-bottom: 1rem;
 `;
+const BackgroundImage = styled.img`
+  position: fixed; 
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: fill; 
+  z-index: 0; 
+  opacity: 0.1; 
+`;
+
+const GrayOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: fill; 
+  background-color: rgba(128, 128, 128, 0.2); 
+`;
 
 const Home: React.FC = () => (
 	<>
-		<Header/>
-		<Container>
-			<Title>Bienvenido a TiUN, tenemos unas sugerencias para ti:</Title>
-			<Carrusel /> {/* Display your carousel component */}
-			<Card/>
-		</Container>
-		<Footer/>
+		<div className=''>
+			<BackgroundImage
+				src={fondo}
+				alt='fondobolsas'
+			/>
+			<GrayOverlay />
+			<Header/>
+			<Container>
+				<Title>Bienvenido a TiUN<br/> tenemos unas sugerencias para ti:</Title>
+				<Carrusel /> {/* Display your carousel component */}
+				<Card Descripcion={'Macbook pro'} Precio={'4000e'}/>
+				<Card Descripcion={'HP XXXX'} Precio={'283929e'}/>
+				<Card Descripcion={'HP XXXX'} Precio={'283929e'}/>
+				<Card Descripcion={'HP XXXX'} Precio={'283929e'}/>
+				<Card Descripcion={'HP XXXX'} Precio={'283929e'}/>
+				<Card Descripcion={'HP XXXX'} Precio={'283929e'}/>
+			</Container>
+			<Footer/>
+		</div>
 	</>
 );
 
