1d51f69672f2edd4d0ef048a8fcda680673174f4
angelvasquez06 <vasquezgerenaangelmateo@gmail.com>
2023-10-08 20:41:26 -0500

    Configuración de firebase para crear productos

--- src/app/my-products/page.tsx
+++ src/app/my-products/page.tsx
@@ -4,6 +4,9 @@ import ReactPaginate from 'react-paginate';
 import Header from '../../components/common/Header';
 import Footer from '../../components/common/Footer';
 import {CRUD} from '../../api/auth';
+import {ref, uploadBytes, getDownloadURL, deleteObject} from 'firebase/storage';
+import {storage} from '../../firebase/config';
+import {useForm} from 'react-hook-form';
 
 const ProductCRUD = () => {
 	const [products, setProducts] = useState([]);
@@ -12,43 +15,65 @@ const ProductCRUD = () => {
 	const [isEditing, setIsEditing] = useState(false);
 	const [editedProduct, setEditedProduct] = useState(null);
 
-	const [newProduct, setNewProduct] = useState({
-		name: '',
-		description: '',
-		price: 0,
-		imageUrl: '',
-		categories: [],
-		stock: 0,
-		discount: 0,
-	});
+	const {handleSubmit, register, reset} = useForm();
 
 	useEffect(() => {
 		loadProducts();
 	}, []);
 
+	const handleAddProduct = async data => {
+		let imageFile = null;
+		let storageRef = null;
+		let imageUrl = null;
+
+		try {
+			// Verifica si se seleccionó una imagen
+			console.log(data)
+			if (data.imageurl?.[0]) {
+				imageFile = data.imageurl[0];
+				storageRef = ref(storage, 'Productos/' + imageFile.name);
+
+				// Sube la imagen a Firebase Storage
+				const snapshot = await uploadBytes(storageRef, imageFile);
+
+				// Obtiene la URL de descarga de la imagen
+				imageUrl = await getDownloadURL(storageRef);
+
+				// Actualiza el campo de imagen en los datos del producto
+				data.imageurl = imageUrl;
+
+				// Realiza la solicitud POST para crear el producto
+				const createdProduct = await CRUD.createProduct(data);
+
+				// Agrega el producto al estado
+				setProducts([...products, createdProduct]);
+
+				// Restablece el formulario y finaliza el proceso de adición
+				setIsAdding(false);
+			}
+		} catch (error) {
+			console.error('Error al agregar el producto:', error);
+			console.log("data despues del error")
+			console.log(data)
+			// Si hubo un error en la carga de la imagen, intenta eliminarla
+			if (imageFile && storageRef) {
+				try {
+					// Intenta eliminar la imagen en caso de error
+					await deleteObject(storageRef);
+					console.log('Imagen eliminada de Firebase Storage debido a un error en la petición POST.');
+					console.log("data despues de borrar")
+					console.log(data)
+				} catch (deleteError) {
+					console.error('No se pudo eliminar la imagen de Firebase Storage:', deleteError);
+				}
+			}
+		}
+	};
+
 	const loadProducts = async () => {
 		try {
 			const products = await CRUD.getProducts();
 			setProducts(products);
-		} catch (error) {
-			console.error(error);
-		}
-	};
-
-	const handleAddProduct = async () => {
-		try {
-			const createdProduct = await CRUD.createProduct(newProduct);
-			setProducts([...products, createdProduct]);
-			setNewProduct({
-				name: '',
-				description: '',
-				price: 0,
-				imageUrl: '',
-				categories: [],
-				stock: 0,
-				discount: 0,
-			});
-			setIsAdding(false);
 		} catch (error) {
 			console.error(error);
 		}
@@ -74,10 +99,10 @@ const ProductCRUD = () => {
 		setIsEditing(false);
 	};
 
-	const handleSaveEdit = async () => {
+	const handleSaveEdit = async data => {
 		try {
 			// Realiza una solicitud PUT para guardar los cambios en el producto
-			const updatedProduct = await CRUD.updateProduct(editedProduct._id, editedProduct);
+			const updatedProduct = await CRUD.updateProduct(editedProduct._id, data);
 			// Actualiza la lista de productos con los cambios
 			const updatedProducts = products.map(product =>
 				product._id === editedProduct._id ? updatedProduct : product,
@@ -107,7 +132,7 @@ const ProductCRUD = () => {
 								isAdding ? 'bg-red-500 text-white' : 'bg-gray-200 text-gray-600'
 							}`}
 						>
-            Mis Productos
+              Mis Productos
 						</button>
 						<button
 							onClick={() => {
@@ -117,75 +142,89 @@ const ProductCRUD = () => {
 								isAdding ? 'bg-gray-200 text-gray-600' : 'bg-red-500 text-white'
 							}`}
 						>
-            Añadir Productos
+              Añadir Productos
 						</button>
 					</div>
 
 					{isAdding ? (
-						<div className='mb-6'>
-							<input
-								type='text'
-								placeholder='Nombre del producto'
-								value={newProduct.name}
-								onChange={e => {
-									setNewProduct({...newProduct, name: e.target.value});
-								}}
-								className='w-full p-2 mb-2 border rounded focus:border-blue-500'
-							/>
+						<form onSubmit={handleSubmit(handleAddProduct)} className='mb-6'>
+							<div>
+								<label>Nombre del producto</label>
+								<input
+									{...register('name')}
+									defaultValue=''
+									className='w-full p-2 mb-2 border rounded focus-border-blue-500'
+								/>
+							</div>
+							<div>
+								<label>Descripción del producto</label>
+								<input
+									{...register('description')}
+									defaultValue=''
+									className='w-full p-2 mb-2 border rounded focus-border-blue-500'
+								/>
+							</div>
+							<div>
+								<label>Precio</label>
+								<input
+									{...register('price')}
+									type='number'
+									defaultValue=''
+									className='w-full p-2 mb-2 border rounded focus-border-blue-500'
+								/>
+							</div>
+							<div>
+								<label>Foto del producto</label>
+								<input
+									{...register('imageurl')}
+									type='file'
+									className='w-full p-2 mb-2 border rounded border-blue-500'
+								/>
+							</div>
 							{/* Resto de los campos de entrada */}
-							<button
-								onClick={handleAddProduct}
-								className='bg-red-500 text-white py-2 px-4 rounded cursor-pointer'
-							>
-              Agregar
+							<button type='submit' className='bg-red-500 text-white py-2 px-4 rounded cursor-pointer'>
+                Agregar
 							</button>
-						</div>
+						</form>
 					) : isEditing ? (
-					// Modo de edición
-						<div className='mb-6'>
-							<input
-								type='text'
-								placeholder='Nombre del producto'
-								value={editedProduct.name}
-								onChange={e => {
-									setEditedProduct({...editedProduct, name: e.target.value});
-								}}
-								className='w-full p-2 mb-2 border rounded focus:border-blue-500'
-							/>
-							<input
-								type='text'
-								placeholder='Descripción del producto'
-								value={editedProduct.description}
-								onChange={e => {
-									setEditedProduct({...editedProduct, description: e.target.value});
-								}}
-								className='w-full p-2 mb-2 border rounded focus:border-blue-500'
-							/>
-							<input
-								type='number'
-								placeholder='Precio'
-								value={editedProduct.price}
-								onChange={e => {
-									setEditedProduct({...editedProduct, price: parseFloat(e.target.value)});
-								}}
-								className='w-full p-2 mb-2 border rounded focus:border-blue-500'
-							/>
-							{/* Agrega campos de edición para otros atributos del producto */}
-							<button
-								onClick={handleSaveEdit}
-								className='bg-green-500 text-white py-2 px-4 rounded cursor-pointer'
-							>
-							Guardar
+						<form onSubmit={handleSubmit(handleSaveEdit)} className='mb-6'>
+							<div>
+								<label>Nombre del producto</label>
+								<input
+									{...register('name')}
+									defaultValue={editedProduct.name}
+									className='w-full p-2 mb-2 border rounded focus-border-blue-500'
+								/>
+							</div>
+							<div>
+								<label>Descripción del producto</label>
+								<input
+									{...register('description')}
+									defaultValue={editedProduct.description}
+									className='w-full p-2 mb-2 border rounded focus-border-blue-500'
+								/>
+							</div>
+							<div>
+								<label>Precio</label>
+								<input
+									{...register('price')}
+									defaultValue={editedProduct.price}
+									type='number'
+									className='w-full p-2 mb-2 border rounded focus-border-blue-500'
+								/>
+							</div>
+							<button type='submit' className='bg-red-500 text-white py-2 px-4 rounded cursor-pointer'>
+                Guardar
 							</button>
 							<button
+								type='button'
 								onClick={handleCancelEdit}
 								className='bg-red-500 text-white py-2 px-4 rounded cursor-pointer ml-2'
 							>
-							Cancelar
+                Cancelar
 							</button>
-						</div>
+						</form>
 					) : (
-					// Modo de visualización
 						<table className='w-full mt-6'>
 							<thead>
 								<tr>
@@ -210,15 +249,15 @@ const ProductCRUD = () => {
 												}}
 												className='bg-red-500 text-white py-2 px-4 rounded cursor-pointer'
 											>
-                      Eliminar
+                        Eliminar
 											</button>
 											<button
 												onClick={() => {
-													handleEditProduct(product); // Activa el modo de edición
+													handleEditProduct(product);
 												}}
 												className='bg-blue-500 text-white py-2 px-4 rounded cursor-pointer ml-2'
 											>
-                      Editar
+                        Editar
 											</button>
 										</td>
 									</tr>
@@ -236,16 +275,16 @@ const ProductCRUD = () => {
 								setCurrentPage(data.selected);
 							}}
 							containerClassName={'flex justify-center items-center my-6'}
-							previousLinkClassName={'border rounded-full p-2 px-4 mx-2 cursor-pointer text-gray-500 hover:bg-gray-200'}
-							nextLinkClassName={'border rounded-full p-2 px-4 mx-2 cursor-pointer text-gray-500 hover:bg-gray-200'}
+							previousLinkClassName={'border rounded-full p-2 px-4 mx-2 cursor-pointer text-gray-500 hover-bg-gray-200'}
+							nextLinkClassName={'border rounded-full p-2 px-4 mx-2 cursor-pointer text-gray-500 hover-bg-gray-200'}
 							disabledClassName={'cursor-not-allowed text-gray-400'}
 							activeClassName={'bg-red-500 text-white rounded-full p-2 px-4 mx-2 cursor-pointer'}
 							pageClassName={'cursor-pointer rounded-full p-2 px-4 mx-2 hover-bg-gray-200'}
 						/>
 					) : null}
 				</div>
-				<Footer />
 			</div>
+			<Footer />
 		</>
 	);
 };
