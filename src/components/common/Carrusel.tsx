f83f502656d21a2d3b044658aaa1bd771bbe74a6
lalopezpa <lalopezpa@unal.edu.co>
2023-10-17 15:19:07 -0500

    imagen promocion y cambios mq

--- src/components/common/Carrusel.tsx
+++ src/components/common/Carrusel.tsx
@@ -1,19 +1,25 @@
 'use client';
 import type React from 'react';
+import Image from 'next/image';
 import {Carousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import sale from '../../assets/images/sale.png';
+import promocion from '../../assets/images/promocion.png';
 
 const Carrusel: React.FC = () => (
-	<div className=' w-3/4 mt-4 mx-auto z-10 bg-inherit border-gray-200 rounded-lg shadow-lg '>
+	<div className='lg:w-3/4 w-full mt-7 mb-5 mx-auto z-10 bg-inherit border-gray-200 rounded-lg shadow-lg'>
 		<Carousel showThumbs={false} showStatus={false}>
 			<div>
-				<img className='rounded-lg' src='https://via.placeholder.com/550x180' alt='Imagen 1' />
+				<Image className='rounded-lg' src={sale} alt='Imagen 1' />
 			</div>
 			<div>
-				<img className='rounded-lg' src='https://via.placeholder.com/550x180' alt='Imagen 2' />
+				<Image className='rounded-lg' src={promocion} alt='Imagen 2' />
 			</div>
 			<div>
-				<img className='rounded-lg' src='https://via.placeholder.com/550x180' alt='Imagen 3' />
+				<Image className='rounded-lg' src={sale} alt='Imagen 3' />
+			</div>
+			<div>
+				<Image className='rounded-lg' src={promocion} alt='Imagen 4' />
 			</div>
 			{/* Agrega más imágenes según sea necesario */}
 		</Carousel>
