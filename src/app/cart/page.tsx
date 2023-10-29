d96afc4b57c015ce2e948e5254e8de9204347300
lalopezpa <lalopezpa@unal.edu.co>
2023-10-29 00:05:30 -0500

    paso del cart a componente

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -41,6 +41,10 @@ const Cart = (): JSX.Element => {
 		return <div> </div>;
 	}
 
+	function data(value: Record<string, unknown>, index: number, array: Array<Record<string, unknown>>): void {
+		throw new Error('Function not implemented.');
+	}
+
 	return (
 		<div className='bg-gris dark:bg-grisOscuro min-h-screen flex flex-col'>
 			<main className='relative flex-1'>
@@ -49,11 +53,31 @@ const Cart = (): JSX.Element => {
 					<div className='grid grid-cols-1 md:grid-cols-2 pb-10 gap-6 container mx-auto p-5 bg-gray-100 max-w-md rounded-lg shadow-md mt-32'>
 						<div>
 							<div className='m-3 p-2 flex-shrink-0'>
-								<div className='m-3 p-2 flex-shrink-0'>
-									{/* Usar join para convertir el array en una cadena */}
-									<h1 className='text-2xl font-bold py-3'>{profile.cart.join(', ')} </h1>
-								</div>
+								{
+									profile.cart.map((item, index) => (
+										item.products.map((product, index) => (
+											<CartItem
+												key={product._id}
+												producto={product}
+												name={product.name}
+												cantidad={item.quantity}
+												precio={product.subtotal}
+												eliminarDelCarrito={async () => Promise.resolve()}
+											/>
+										))
+									))
+								}
 							</div>
+
+							<div>
+								{
+									profile.cart.map((item, index) => (
+										<div>{JSON.stringify(item)}</div>
+									))
+								}
+							</div>
+
+
 						</div>
 					</div>
 				</div>
--- src/components/common/CartItem.tsx
+++ src/components/common/CartItem.tsx
@@ -4,44 +4,46 @@ import {type ProductType} from '../../types/CRUD/ProductSchema';
 
 type CartItemProps = {
 	producto: ProductType;
+	name: string;
 	cantidad: number;
 	precio: number;
 	eliminarDelCarrito: (productoId: string) => void;
 };
+// ... (importa lo necesario)
 
-const CartItem: React.FC<CartItemProps> = ({producto, cantidad, precio, eliminarDelCarrito}) => {
+const CartItem: React.FC<CartItemProps> = ({producto, name, cantidad, precio, eliminarDelCarrito}) => {
 	const [nuevaCantidad, setNuevaCantidad] = useState(cantidad);
 
 	const handleCantidadChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const nuevaCantidad = parseInt(event.target.value, 10);
-		setNuevaCantidad(nuevaCantidad);
+	  const nuevaCantidad = parseInt(event.target.value, 10);
+	  setNuevaCantidad(nuevaCantidad);
 	};
 
 	return (
-		<>
-			<div className='flex items-center justify-between border-b p-4'>
-				<div className='flex items-center space-x-4'>
-					<img src={producto.imageUrl} alt={producto.name} className='w-12 h-12 object-cover' />
+	  <div className='flex items-center justify-between border-b p-4'>
+			{/* Usamos el producto._id como clave única */}
+			<div key={producto._id} className='flex items-center space-x-4'>
+		  <img src={producto.imageUrl} alt={producto.name} className='w-12 h-12 object-cover' />
+		  <div>
+					<p className='font-bold'>{producto.name}</p>
 					<div>
-						<p className='font-bold'>{producto.name}</p>
-						<div>
-							<p>Cantidad: {cantidad}</p>
-							{/* Aquí puedes agregar la lógica para cambiar la cantidad */}
-							<input type='number' value={nuevaCantidad} onChange={handleCantidadChange} />
-						</div>
-						<p>Precio: ${precio.toFixed(2)}</p>
+			  <p>Cantidad: {cantidad}</p>
+			  {/* Aquí puedes agregar la lógica para cambiar la cantidad */}
+			  <input type='number' value={nuevaCantidad} onChange={handleCantidadChange} />
 					</div>
-				</div>
-				<button
-					onClick={() => {
-						eliminarDelCarrito(producto._id);
-					}}
-					className='text-red-500'>
-          Eliminar
-				</button>
+					<p>Precio: ${precio.toFixed(2)}</p>
+		  </div>
 			</div>
-		</>
+			<button
+		  onClick={() => {
+					eliminarDelCarrito(producto._id);
+		  }}
+		  className='text-red-500'>
+		  Eliminar
+			</button>
+	  </div>
 	);
 };
 
 export default CartItem;
+
